ad2c69b5a8df265ce86802ba63c87390189adb19
ebnr321130921-pixel <ebnr321130921@gmail.com>
2025-11-24 17:52:18 +0900

    Add files via upload

---
+++ daily_fetch.py
@@ -0,0 +1,8 @@
+# Placeholder daily fetch script
+# Real API endpoints will be added when data source confirmed
+
+import pandas as pd
+
+html = "<html><body><h1>Daily Fetch Placeholder</h1></body></html>"
+with open("portfolio_A.html", "w") as f:
+    f.write(html)
